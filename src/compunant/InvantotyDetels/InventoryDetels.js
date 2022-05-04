b411f0221151acdac1f517da0c5ae6079c0bae61
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-05-04 22:41:13 +0600

    sabbir vai

--- src/compunant/InvantotyDetels/InventoryDetels.js
+++ src/compunant/InvantotyDetels/InventoryDetels.js
@@ -1,24 +1,51 @@
+
 import React, { useEffect, useState } from 'react';
+import Feedback from 'react-bootstrap/esm/Feedback';
 import { useParams } from 'react-router-dom';
 import './InventoryDetels.css';
 
 
 const InventoryDetels = () => {
     const [product,setProduct]=useState([]);
-    useEffect(()=>{
-        fetch('https://afternoon-shelf-86767.herokuapp.com/product')
+    const {id}=useParams();
+
+    async function getAndUpdateProduct(id){
+        return fetch(`https://afternoon-shelf-86767.herokuapp.com/product/${id}`)
         .then(res=>res.json())
         .then(data=>setProduct(data))
-    },[]);
-    // const [inventory,setInventory]=useState({})
-       const {id}=useParams();
-      const result=product.find(pro=>pro._id ===id);
+    }
+
+    useEffect( ()=>{
+      getAndUpdateProduct(id)
+    },[id]);
+
+
+     const [inventory,setInventory]=useState({});
+      
+   
         // setInventory(result)
         // console.log(inventory);
+
       const deliverd=()=>{
-        const Remaining = parseFloat( result?.quantity-1);
-
-
+        fetch(`https://afternoon-shelf-86767.herokuapp.com/product/reduce/${id}`,{method:"PUT"})
+        .then(res=>res.json())
+        .then(data=>
+            getAndUpdateProduct(id)
+            )
+      }
+      const deliverdIncrease=()=>{
+          const inputValue=parseFloat( document.getElementById("increse").value);
+        
+        
+        fetch(`https://afternoon-shelf-86767.herokuapp.com/product/increase/${id}`,{method:"PUT",  headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          }, body:`{"amount":${inputValue ||2}}`})
+        .then(res=>res.json())
+        .then(data=>
+            getAndUpdateProduct(id)
+            )
+            
       }
          
      
@@ -27,15 +54,21 @@ const InventoryDetels = () => {
             <h1 className='text-center p-5'>Product Deteles</h1>
             <div className='eachitem inventory-ubdate mx-auto'>
             <div className='img'>
-                <img src={result?.picture} alt="img" />
+                <img src={product?.picture} alt="img" />
             </div>
             <div className='each-deteles '>
-                <h3>Name :{result?.name}</h3>
-                <h4>Price :{result?.price}</h4>
-                <h4> Supliyar :{result?.supliyer}</h4>
-                <h4>Quattity :{result?.quantity}</h4>
-                <p> Description :{result?.description}</p>
+                <h3>Name :{product?.name}</h3>
+                <h4>Price :{product?.price}</h4>
+                <h4> Supliyar :{product?.supliyer}</h4>
+                <h4>Quattity :{product?.quantity}</h4>
+                <p> Description :{product?.description}</p>
                 <button onClick={deliverd}> delivered</button>
+                <br />
+                <label htmlFor=""> Add Quentity</label>
+                <br />
+                <br />
+                <input type="text" id="increse" />
+                <button onClick={deliverdIncrease}>devebard</button>
             </div>
             
            
@@ -47,3 +80,175 @@ const InventoryDetels = () => {
 };
 
 export default InventoryDetels;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import Feedback from 'react-bootstrap/esm/Feedback';
+// import { useParams } from 'react-router-dom';
+// import './InventoryDetels.css';
+
+
+// const InventoryDetels = () => {
+//     const [product,setProduct]=useState([]);
+//     const {id}=useParams();
+
+//     async function getAndUpdateProduct(id){
+//         return fetch(https://afternoon-shelf-86767.herokuapp.com/product/${id})
+//         .then(res=>res.json())
+//         .then(data=>setProduct(data))
+//     }
+
+//     useEffect(async ()=>{
+//       await getAndUpdateProduct(id)
+//     },[id]);
+
+
+//     // const [inventory,setInventory]=useState({})
+      
+//       const result=product.find(pro=>pro._id ===id);
+//         // setInventory(result)
+//         // console.log(inventory);
+//       const deliverd=()=>{
+//         fetch(https://afternoon-shelf-86767.herokuapp.com/product/reduce/${id})
+//         .then(res=>res.json())
+//         .then(data=>
+//             await getAndUpdateProduct(id)
+//             )
+//       }
+         
+     
+//     return (
+//         <div>
+//             <h1 className='text-center p-5'>Product Deteles</h1>
+//             <div className='eachitem inventory-ubdate mx-auto'>
+//             <div className='img'>
+//                 <img src={result?.picture} alt="img" />
+//             </div>
+//             <div className='each-deteles '>
+//                 <h3>Name :{result?.name}</h3>
+//                 <h4>Price :{result?.price}</h4>
+//                 <h4> Supliyar :{result?.supliyer}</h4>
+//                 <h4>Quattity :{result?.quantity}</h4>
+//                 <p> Description :{result?.description}</p>
+//                 <button onClick={deliverd}> delivered</button>
+//             </div>
+            
+           
+            
+//         </div>
+            
+//         </div>
+//     );
+// };
+
+// export default InventoryDetels;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import React, { useEffect, useState } from 'react';
+// // import Feedback from 'react-bootstrap/esm/Feedback';
+// // import { useParams } from 'react-router-dom';
+// // import './InventoryDetels.css';
+
+
+// // const InventoryDetels = () => {
+// //     const [product,setProduct]=useState([]);
+// //     useEffect(()=>{
+// //         fetch('https://afternoon-shelf-86767.herokuapp.com/product')
+// //         .then(res=>res.json())
+// //         .then(data=>setProduct(data))
+// //     },[]);
+// //     // const [inventory,setInventory]=useState({})
+// //        const {id}=useParams();
+// //       const result=product.find(pro=>pro._id ===id);
+// //         // setInventory(result)
+// //         // console.log(inventory);
+// //       const deliverd=()=>{
+// //         fetch(`https://afternoon-shelf-86767.herokuapp.com/product/reduce/${id}`)
+// //         .then(res=>res.json())
+// //         .then(data=>)
+
+
+// //       }
+         
+     
+// //     return (
+// //         <div>
+// //             <h1 className='text-center p-5'>Product Deteles</h1>
+// //             <div className='eachitem inventory-ubdate mx-auto'>
+// //             <div className='img'>
+// //                 <img src={result?.picture} alt="img" />
+// //             </div>
+// //             <div className='each-deteles '>
+// //                 <h3>Name :{result?.name}</h3>
+// //                 <h4>Price :{result?.price}</h4>
+// //                 <h4> Supliyar :{result?.supliyer}</h4>
+// //                 <h4>Quattity :{result?.quantity}</h4>
+// //                 <p> Description :{result?.description}</p>
+// //                 <button onClick={deliverd}> delivered</button>
+// //             </div>
+            
+           
+            
+// //         </div>
+            
+// //         </div>
+// //     );
+// // };
+
+// // export default InventoryDetels;
